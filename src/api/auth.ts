34a33133cdfd4db064e0f9f04479e39c6ec5b0ac
zyhsu <jenny010328@gmail.com>
2023-12-28 20:45:51 +0800

    feat : change axios to useFetch and setting interceptor

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,7 @@
-import { req } from "./https";
+// import { req } from "./https";
+
+import { resultType } from ".";
+import { HttpOption } from "@/composables/useHttps";
 
 interface loginData {
   email: string;
@@ -10,11 +13,31 @@ interface registerData {
   password: string;
 }
 
-export const Auth = {
-  apiPostLogin(data: loginData) {
-    return req("post", "/login", data);
-  },
-  apiPostRegister(data: registerData) {
-    return req("post", "/register", data);
-  },
+// export const Auth = {
+//   apiPostLogin(data: loginData) {
+//     return req("post", "/login", data);
+//   },
+//   apiPostRegister(data: registerData) {
+//     return req("post", "/register", data);
+//   },
+// };
+
+enum AuthApi {
+  login = "/login",
+  register = "/register",
+}
+
+// option就是useFetch的选项参数
+export const apiPostLogin = async (
+  params: loginData,
+  option?: HttpOption<resultType>
+) => {
+  return await useHttp.post<resultType>(AuthApi.login, params, option);
+};
+
+export const apiPostRegister = async (
+  params: registerData,
+  option?: HttpOption<resultType>
+) => {
+  return await useHttp.post<resultType>(AuthApi.register, params, option);
 };
